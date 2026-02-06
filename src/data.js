0a63244268914a75ef61414ca43653331a5cbc89
Maximiliano Valli <127133282+maxivalli@users.noreply.github.com>
2026-02-06 09:59:14 -0300

    load menu

--- src/data.js
+++ src/data.js
@@ -1,38 +1,43 @@
 export const SECCIONES_RIGO = [
   {
     id: 1,
-    title: "CONSTRUCCIÓN",
-    badge: "Obra Gruesa",
-    image: "https://images.unsplash.com/photo-1504307651254-35680f356dfd?q=80&w=800",
-    colorOverlay: "bg-zinc-900",
-    description: "Materiales pesados para cimientos indestructibles.",
-    featured: false
+    title: "CAFÉ & DECO",
+    badge: "Experiencia",
+    image:
+      "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?q=80&w=1200",
+    colorOverlay: "bg-orange-950",
+    description:
+      "El punto de encuentro donde nacen las mejores ideas. Vení por los materiales, quedate por el aroma.",
+    featured: true,
   },
   {
     id: 2,
-    title: "FERRETERÍA",
-    badge: "Herramientas",
-    image: "https://images.unsplash.com/photo-1581783898377-1c85bf937427?q=80&w=1015&auto=format&fit=crop",
-    colorOverlay: "bg-rigo-clay",
-    description: "Herramientas de precisión para cada detalle.",
-    featured: false
+    title: "CONSTRUCCIÓN",
+    badge: "Obra Gruesa",
+    image:
+      "https://images.unsplash.com/photo-1504307651254-35680f356dfd?q=80&w=800",
+    colorOverlay: "bg-zinc-900",
+    description: "Materiales pesados para cimientos indestructibles.",
+    featured: false,
   },
   {
     id: 3,
-    title: "SHOWROOM",
-    badge: "Todo para tu casa",
-    image: "https://images.unsplash.com/photo-1584622650111-993a426fbf0a?q=80&w=800",
-    colorOverlay: "bg-stone-600",
-    description: "Grifería de autor y revestimientos que definen tu estilo.",
-    featured: false
+    title: "FERRETERÍA",
+    badge: "Herramientas",
+    image:
+      "https://images.unsplash.com/photo-1581783898377-1c85bf937427?q=80&w=1015&auto=format&fit=crop",
+    colorOverlay: "bg-rigo-clay",
+    description: "Herramientas de precisión para cada detalle.",
+    featured: false,
   },
   {
     id: 4,
-    title: "CAFÉ & DECO",
-    badge: "Experiencia",
-    image: "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?q=80&w=1200",
-    colorOverlay: "bg-orange-950",
-    description: "El punto de encuentro donde nacen las mejores ideas. Vení por los materiales, quedate por el aroma.",
-    featured: true
-  }
+    title: "SHOWROOM",
+    badge: "Todo para tu casa",
+    image:
+      "https://images.unsplash.com/photo-1584622650111-993a426fbf0a?q=80&w=800",
+    colorOverlay: "bg-stone-600",
+    description: "Grifería de autor y revestimientos que definen tu estilo.",
+    featured: false,
+  },
 ];
